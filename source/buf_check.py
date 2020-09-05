27d83b2f043f105da589d9c141c81658c9df2c52
run3 <53053198+itsjeffersonli@users.noreply.github.com>
2020-09-05 21:58:05 +0800

    Create buf_check.py

---
+++ source/buf_check.py
@@ -0,0 +1,28 @@
+import socket
+import time
+import sys
+
+ip = str(sys.argv[1])
+port = str(sys.argv[2])
+timeout = 5
+
+buffer = []
+counter = 100
+while len(buffer) < 200:
+    buffer.append("A" * counter)
+    counter += 100
+
+for string in buffer:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        connect = s.connect((ip, port))
+        s.recv(1024)
+        print("Fuzzing with %s bytes" % len(string))
+        s.send(str(sys.argv[3])+ " " + string + "\r\n")
+        s.recv(1024)
+        s.close()
+    except:
+        print("Could not connect to " + ip + ":" + str(port))
+        sys.exit(0)
+    time.sleep(1)
